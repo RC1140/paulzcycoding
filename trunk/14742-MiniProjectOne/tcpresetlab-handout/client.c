75d26889cd14d9907a16d39a99c69f9c5f22ca05
paulzcy <paulzcy@70f2d01e-b08b-11de-a3a9-b9053a4ef86d>
2010-02-27 17:08:44 +0000

    tcp-reset rewrite

--- trunk/14742-MiniProjectOne/tcpresetlab-handout/client.c
+++ trunk/14742-MiniProjectOne/tcpresetlab-handout/client.c
@@ -1,134 +1,107 @@
-/*****************************************************************
- * File: client.c
- * by: Group YaKeXi
- * 
- * History:
- * 
- *  2010/02/22 Completed and tested by Hongkai Xing 
- *             on Ubuntu 9.10 with libnet 1.1.4 and libpcap 0.8.
- *
- *****************************************************************/
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <unistd.h>
 #include <netdb.h>
-/*
- * Simple tcp client for use in tcp reset attack testing.
- *
- * Execute as follows: ./client  server_address server_port client_port input_file
- *
- */
-static const int CLIBUFFER = 1024;
-static const int CLISENT_MAX = 800 * 1024;//800 kilobytes
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-void showErrorMsg(char *msg)
-{
-        perror(msg);
-        exit(1);
+#define BUFFERSIZE 1024
+#define SLEEPTIME 1
+
+void usage() {
+	fprintf(stderr, "client <server ip> <server port> <client port> <file>\n");
 }
 
-int main(int argc, char **argv)
-{	
-	char *cli_port_str;
-	char *srv_addr_str;
-	char *srv_port_str;
-	char *input_file_str; 
-
-	if(argc >= 4) {
-		cli_port_str = argv[3];
-	} else {
-		cli_port_str = "10001";
-	}
-  
-	if (argc >= 2) {
-		srv_addr_str = argv[1];
-	} else {
-		srv_addr_str = "127.0.0.1";
-	}
-  
-	if (argc >= 3) {
-		srv_port_str = argv[2];
-	}
-	else {
-		srv_port_str = "9999";
+int main(int argc, char* argv[]) {
+	if (argc != 5) {
+		usage();
+		exit(EXIT_FAILURE);
 	}
 
-	if(argc >= 5) {
-		input_file_str = argv[4];
-	} else {
-		input_file_str = "junk";
-	}
+	char* serv_addr = argv[1];
+	char* serv_port = argv[2];
+	char* cli_port = argv[3];
+	char* file = argv[4];
 
-	printf("starting client using port %s, connecting to server %s:%s, reading file %s\n",
-		cli_port_str, srv_addr_str, srv_port_str, input_file_str);
+	struct addrinfo hints;
+	struct addrinfo *srv_info, *cli_info;
 
-	/*******************/
-	int sock_fd,
-	    srv_port_int,
-	    rv,
-	    cli_sent;
-
-	struct in_addr srv_ipv4addr;	//ipv4 address structure for server
-	struct sockaddr_in 
-		srv_addr,	//internet address structure for server
-		cli_addr;	//internet address structure for client
-	struct hostent *srv_host;	//pointer to internet protocol host (server)
-
-	char cli_buffer[CLIBUFFER];
-	FILE *input_file;
-
-	srv_port_int = atoi(srv_port_str);
-        if ((sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-		showErrorMsg("Client cannot open socket\n");
-
-	inet_pton(AF_INET, srv_addr_str, &srv_ipv4addr);
-	srv_host = gethostbyaddr(&srv_ipv4addr,sizeof(srv_ipv4addr),AF_INET);
-	if (srv_host == NULL)
-		showErrorMsg("Client cannot find the server host\n");
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
 	
-        bzero((char*)&srv_addr,sizeof(srv_addr));       //zero out the structure
-        srv_addr.sin_family=AF_INET;                    //set to IPv4 address family
-	bcopy((char*)srv_host->h_addr, (char*)&srv_addr.sin_addr.s_addr, srv_host->h_length);		srv_addr.sin_port=htons(srv_port_int);  	//host byte order-->network byte order
-	
-	// to specifiy the source port
-	cli_addr.sin_family=AF_INET;
-	cli_addr.sin_port=htons(atoi(cli_port_str));
-	cli_addr.sin_addr.s_addr=INADDR_ANY;
-	if (bind(sock_fd,(struct sockaddr*)&cli_addr,sizeof(cli_addr))<0)
-		showErrorMsg("Client cannot bind socket");
+	if (getaddrinfo(serv_addr, serv_port, &hints, &srv_info) == -1) {
+		perror("getaddrinfo failed");
+		exit(EXIT_FAILURE);
+	}
 
-	// connect to the server
-	if (connect(sock_fd, (struct sockaddr*)&srv_addr,sizeof(srv_addr))<0)
-		showErrorMsg("Client cannot connect to server\n");
-	bzero(cli_buffer,CLIBUFFER);
+	hints.ai_family = AF_INET;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
-	// try to open the file
-	if ((input_file=fopen(input_file_str,"r"))==NULL)
-		showErrorMsg("Client cannot open file\n");
+	if (getaddrinfo(NULL, cli_port, &hints, &cli_info) == -1) {
+		perror("getaddrinfo failed");
+		exit(EXIT_FAILURE);
+	}
+
+	int sock_fd;
+
+	sock_fd = socket(srv_info->ai_family, srv_info->ai_socktype, srv_info->ai_protocol);
+	if (sock_fd == -1) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (bind(sock_fd, cli_info->ai_addr, cli_info->ai_addrlen) == -1) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+
+//	setsockopt(sock_fd, SOL_SOCKET, SO_SNDBUF, &window_size, sizeof(window_size));
+//	setsockopt(sock_fd, SOL_SOCKET, SO_RCVBUF, &window_size, sizeof(window_size));
 	
-	// try to send the file
-	cli_sent=0;
-	while(fgets(cli_buffer,CLIBUFFER,input_file)!=NULL){
-		rv=write(sock_fd,cli_buffer,strlen(cli_buffer));
-		printf("%d\n",rv);
-		if (rv<0)
-			 showErrorMsg("Client cannot write to socket\n");
-		cli_sent+=rv;
-		if (cli_sent > CLISENT_MAX)
+	if (connect(sock_fd, srv_info->ai_addr, srv_info->ai_addrlen) == -1) {
+		perror("connect failed");
+		exit(EXIT_FAILURE);
+	}
+
+	freeaddrinfo(srv_info);
+	freeaddrinfo(cli_info);
+	
+	FILE* filehd = fopen(file, "r");
+	if (filehd == NULL) {
+		perror("fopen failed");
+		exit(EXIT_FAILURE);
+	}
+
+#ifdef DEBUG 
+	int window_size = 1024 * 128;
+	socklen_t opt_size = sizeof(window_size);
+	getsockopt(sock_fd, SOL_SOCKET, SO_SNDBUF, &window_size, &opt_size);
+	fprintf(stderr, "Peeked Send Window Size: %d\n", window_size);
+	getsockopt(sock_fd, SOL_SOCKET, SO_RCVBUF, &window_size, &opt_size);
+	fprintf(stderr, "Peeked Recv Window Size: %d\n", window_size);
+#endif
+
+	char buffer[BUFFERSIZE];
+	int rv;
+
+	while (fgets(buffer, BUFFERSIZE, filehd) != NULL) {
+//		rv = send(sock_fd, buffer, strlen(buffer), 0);
+		rv = write(sock_fd, buffer, strlen(buffer));
+		printf("%d\n", rv);
+		if (rv <= 0) {
+			perror("send failed");
 			break;
-		bzero(cli_buffer,CLIBUFFER);
-		sleep(1);
+		}
+		sleep(SLEEPTIME);
 	}
 
-	fclose(input_file);
+	fclose(filehd);
 	close(sock_fd);
-	return 0;  
+	return 0;
 }
-
--- trunk/14742-MiniProjectOne/tcpresetlab-handout/server.c
+++ trunk/14742-MiniProjectOne/tcpresetlab-handout/server.c
@@ -1,108 +1,112 @@
-/*****************************************************************
- * File: server.c
- * by: Group YaKeXi
- * 
- * History:
- * 
- *  2010/02/22 Completed and tested by Hongkai Xing 
- *             on Ubuntu 9.10 with libnet 1.1.4 and libpcap 0.8.
- *
- *
- ****************************************************************/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
-/*
- * Simple tcp server to test the tcp reset attack.
- *
- * Start as follows: ./server  server_ip  server_port
- *
- */
+#define BACKLOG 1
+#define BUFFERSIZE 65536
 
-static const int MAXPENDING =5;	//maximum connection request
-static const int SRVBUFFER = 65536;
-
-void showErrorMsg(char *msg)
-{
-	perror(msg);
-	exit(1);
+void usage() {
+	fprintf(stderr, "server <serverip> <serverport>\n");
 }
 
+int main(int argc, char* argv[]) {
 
-int main(int argc, char **argv) {
-	char *srv_addr_str;	//server address in string
-	char *srv_port_str;	//server port number in string
-
-	if (argc >= 2) {	//get or set server ip address
-		srv_addr_str = argv[1];
-	} else {
-		srv_addr_str = "127.0.0.1";
+	if (argc != 3) {
+		usage();
+		exit(EXIT_FAILURE);
 	}
 
-	if (argc >= 3) {	//get or set server port number
-		srv_port_str = argv[2];
-	} else {
-		srv_port_str = "9999";
+	char* srv_addr = argv[1];
+	char* srv_port = argv[2];
+#ifdef DEBUG
+	fprintf(stderr, "addr: %s\nport: %s\n", srv_addr, srv_port);
+#endif
+
+	struct addrinfo hints;
+	struct addrinfo *srv_info, *p;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET; 
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+
+	if (getaddrinfo(srv_addr, srv_port, &hints, &srv_info) == -1) {
+		perror("getaddrinfo error");
+		exit(EXIT_FAILURE);
 	}
 
-	printf("starting server at IP %s, listening on port %s\n", srv_addr_str, srv_port_str);
-
-
-	/*******************/
-        int sock_fd,            //socket used in bind and listen function
-            new_sock_fd,        //socket used in accept and read function
-            srv_port_int,       //server port number in integer
-            cli_len,            //size of cli_addr
-            rv;                 //return value of read and write function
-        char srv_buffer[SRVBUFFER];     //server's receive buffer
-        struct sockaddr_in
-                srv_addr,       //internet address structure for server
-                cli_addr;       //internet address structure for client
-
-
-
-	if ((sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) 
-		showErrorMsg("Server cannot open socket\n");
+	int serv_sock_fd;
 	
-	srv_port_int=atoi(srv_port_str);
-	bzero((char*)&srv_addr,sizeof(srv_addr));	//zero out the structure
-	srv_addr.sin_family=AF_INET;			//set to IPv4 address family
-	srv_addr.sin_addr.s_addr=inet_addr(srv_addr_str);	//specific interface
-	srv_addr.sin_port=htons(srv_port_int);	//host byte order --> network byte order
-	
-
-	if (bind(sock_fd, (struct sockaddr*)&srv_addr,sizeof(srv_addr))<0)
-		showErrorMsg("Server cannot bind socket\n");
-	if (listen(sock_fd,MAXPENDING)<0)
-		showErrorMsg("Server cannot listen to socket\n");
-	
-	
-	while(1){
-		cli_len = sizeof(cli_addr);	//get the length of client address structure
-		new_sock_fd = accept(sock_fd,(struct sockaddr *)&cli_addr,(socklen_t*)&cli_len);
-		if (new_sock_fd < 0)
-			showErrorMsg("Server cannot accept\n");
-		rv=1;
-		while(rv>0){
-			bzero(srv_buffer, SRVBUFFER);
-	                rv=recv(new_sock_fd,srv_buffer,SRVBUFFER-1,0);
-                	printf("%d\n",rv);
-			if (rv<0)
-        	                showErrorMsg("Server cannot read from socket\n");
+	for (p = srv_info; p != NULL; p = p->ai_next) {
+#ifdef DEBUG
+		fprintf(stderr, "%d\n", p->ai_addr->sa_family);
+#endif
+		serv_sock_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (serv_sock_fd == -1) {
+			continue;
 		}
-		shutdown(new_sock_fd,2);
-		close(new_sock_fd);	//close the client's socket
+#ifdef DEBUG
+		fprintf(stderr, "socket created: %d\n", serv_sock_fd);
+#endif
+
+		if (bind(serv_sock_fd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(serv_sock_fd);
+			continue;
+		}
+
+		break;
 	}
+
+	if (p == NULL) {
+		perror("socket or bind error");
+		exit(EXIT_FAILURE);
+	}
+
+	//int window_size = 128 * 1024;	/* 128 kilobytes */
+	//setsockopt(serv_sock_fd, SOL_SOCKET, SO_SNDBUF, &window_size, sizeof(window_size));
+	//setsockopt(serv_sock_fd, SOL_SOCKET, SO_RCVBUF, &window_size, sizeof(window_size));
+
+	if (listen(serv_sock_fd, BACKLOG) == -1) {
+		perror("listen error");
+		exit(EXIT_FAILURE);
+	}
+#ifdef DEBUG
+	fprintf(stderr, "start listening on %s:%s\n", srv_addr, srv_port);
+#endif
+
+	struct sockaddr_storage cli_addr;
+	socklen_t addr_size = sizeof(cli_addr);
+	int new_sock_fd = accept(serv_sock_fd, (struct sockaddr *)&cli_addr, &addr_size);
+
+	if (new_sock_fd == -1) {
+		perror("accept failed");
+		exit(EXIT_FAILURE);
+	}
+#ifdef DEBUG
+	fprintf(stderr, "new connection established\n");
+#endif
+	
+	char buffer[BUFFERSIZE];
+	int rv;
+
+	while (1) {
+//		rv = recv(new_sock_fd, buffer, BUFFERSIZE, 0);
+		rv = read(new_sock_fd, buffer, BUFFERSIZE);
+		printf("%d\n", rv);
+		if (rv <= 0) {
+			perror("read error");
+			break;
+		}
+	}
+
+	close(new_sock_fd);
+	close(serv_sock_fd);
+
+	freeaddrinfo(srv_info);
 
 	return 0;
 }
-
-
-
