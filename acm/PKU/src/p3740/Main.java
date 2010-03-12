85415050c3a07fed1d6d872648047554b8f55ff9
paulzcy <paulzcy@70f2d01e-b08b-11de-a3a9-b9053a4ef86d>
2010-03-12 19:43:23 +0000

    Problem 3740, Java TLE but C++ AC

--- acm/PKU/src/p3740/Main.java
+++ acm/PKU/src/p3740/Main.java
@@ -7,35 +7,45 @@ public class Main {
 	static final String NOTFOUND = "It is impossible";
 	static final int[] E = { 0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100,
 			0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000 };
+	
+	static final int MAXN = 300;
+	static final int MAXM = 16;
+	
+	static int[] matrix = new int[MAXN];
+	static int[] next = new int[(1 << MAXM) + 1];
 
 	public static void main(String[] args) {
 		Scanner jin = new Scanner(System.in);
+		
 		while (jin.hasNext()) {
 			int m = jin.nextInt();
 			int n = jin.nextInt();
 
-			int[] matrix = new int[n];
-
+			for (int i = 0; i < n; ++i) {
+				matrix[i] = 0;
+			}
+			
 			for (int i = 0; i < m; ++i) {
 				for (int j = 0; j < n; ++j) {
 					matrix[j] |= jin.nextInt() * E[i];
 				}
 			}
+			
+			int HEAD = 1 << m;
 
-			int[] next = new int[(1 << m) + 1];
-			for (int i = 0; i < 1 << m; ++i) {
+			for (int i = 0; i < HEAD; ++i) {
 				next[i] = i + 1;
 			}
-			next[1 << m] = 0;
+			next[HEAD] = 0;
 
 			int k;
 			for (k = 0; k < n; ++k) {
-				int p = 1 << m;
-				int q = next[1 << m];
+				int p = HEAD;
+				int q = next[p];
 				if (p == q)
 					break;
 
-				while (q != 1 << m) {
+				while (q != HEAD) {
 					int x = q & matrix[k];
 					if (x > 0 && (x & (x - 1)) == 0) {
 						p = q;
---
+++ acm/PKUC++/p3740/p3740.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+const char FOUND[] = "Yes, I found it";
+const char NOTFOUND[] = "It is impossible";
+const int E[] = { 0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100, 0x200,
+		0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000 };
+
+const int MAXN = 300;
+const int MAXM = 16;
+
+int matrix[MAXN];
+int next[(1 << MAXM) + 1];
+
+int main(int argc, char* argv[]) {
+
+	int m, n;
+	while (cin >> m >> n) {
+
+		for (int i = 0; i < n; ++i) {
+			matrix[i] = 0;
+		}
+
+		for (int i = 0; i < m; ++i) {
+			for (int j = 0; j < n; ++j) {
+				int t;
+				cin >> t;
+				matrix[j] |= t * E[i];
+			}
+		}
+
+		int HEAD = 1 << m;
+
+		for (int i = 0; i < HEAD; ++i) {
+			next[i] = i + 1;
+		}
+		next[HEAD] = 0;
+
+		int k;
+		for (k = 0; k < n; ++k) {
+			int p = HEAD;
+			int q = next[p];
+			if (p == q)
+				break;
+
+			while (q != HEAD) {
+				int x = q & matrix[k];
+				if (x > 0 && (x & (x - 1)) == 0) {
+					p = q;
+				} else {
+					next[p] = next[q];
+				}
+				q = next[p];
+			}
+		}
+
+		if (k == n) {
+			cout << FOUND << endl;
+		} else {
+			cout << NOTFOUND << endl;
+		}
+	}
+	return 0;
+}
