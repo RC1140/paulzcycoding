8bb7e1e90c4aa7dfcc5e48f576a77163e6d23fd0
paulzcy <paulzcy@70f2d01e-b08b-11de-a3a9-b9053a4ef86d>
2010-03-16 03:32:44 +0000

    2000 & 2352

---
+++ trunk/acm/p2352/p2352.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+	const int MAXN = 15000;
+	const int MAXX = 65535;
+
+	int sum[MAXN + 1];
+	int tree[MAXX + 1];
+	int n;
+	int i, j, k;
+	int x, y;
+
+	while (scanf("%d", &n) == 1) {
+		memset(sum, 0, sizeof(sum));
+		memset(tree, 0, sizeof(tree));
+
+		for (i = 0; i < n; ++i) {
+			scanf("%d %d", &x, &y);
+
+			k = 0;
+			for (j = x + 1; j > 0; j -= (j & -j)) {
+				k += tree[j];
+			}
+			++sum[k];
+			for (j = x + 1; j <= MAXX; j += (j & -j)) {
+				++tree[j];
+			}
+		}
+		for (i = 0; i < n; ++i) {
+			printf("%d\n", sum[i]);
+		}
+	}
+
+	return 0;
+}
